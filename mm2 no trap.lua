75fe7c5bad36b65689d1b8dc8dcadc937398a880
Maverick0780 <80007920+Maverick0780@users.noreply.github.com>
2021-04-10 09:00:58 -0400

    Add files via upload

---
+++ mm2 no trap.lua
@@ -0,0 +1,17 @@
+wait(25)
+if game.PlaceId == 142823291 then
+game.Player.LocalPlayer.PlayerScripts.TrapScriptClient.TrapGUI:Destroy();
+game.ReplicatedStorage.TrapSystem.TrapReplicate:Destroy();
+game.ReplicatedStorage.TrapSystem.TrapHitLocal:Destroy();
+game.ReplicatedStorage.TrapSystem.TrapHitVisual:Destroy();
+elseif game.PlaceId == 335132309 then
+game.Player.LocalPlayer.PlayerScripts.TrapScriptClient.TrapGUI:Destroy();
+game.ReplicatedStorage.TrapSystem.TrapReplicate:Destroy();
+game.ReplicatedStorage.TrapSystem.TrapHitLocal:Destroy();
+game.ReplicatedStorage.TrapSystem.TrapHitVisual:Destroy();
+elseif game.PlaceId == 636649648 then
+game.Player.LocalPlayer.PlayerScripts.TrapScriptClient.TrapGUI:Destroy();
+game.ReplicatedStorage.TrapSystem.TrapReplicate:Destroy();
+game.ReplicatedStorage.TrapSystem.TrapHitLocal:Destroy();
+game.ReplicatedStorage.TrapSystem.TrapHitVisual:Destroy();
+end
---
+++ mm2 press backslash for traps at mouse cursor.lua
@@ -0,0 +1,40 @@
+wait(25)
+if game.PlaceId == 142823291 then
+local plr = game.Players.LocalPlayer
+local mouse = plr:GetMouse()
+local UIS = game:GetService("UserInputService")
+function trap(x,y,z)
+game:GetService("ReplicatedStorage").TrapSystem.PlaceTrap:InvokeServer(CFrame.new(Vector3.new(x, y, z)))
+end
+
+UIS.InputBegan:Connect(function(input)
+if input.KeyCode == Enum.KeyCode.BackSlash then
+local pos = mouse.hit.p
+trap(pos.X, pos.Y, pos.Z)
+end end)
+elseif game.PlaceId == 335132309 then
+    local plr = game.Players.LocalPlayer
+local mouse = plr:GetMouse()
+local UIS = game:GetService("UserInputService")
+function trap(x,y,z)
+game:GetService("ReplicatedStorage").TrapSystem.PlaceTrap:InvokeServer(CFrame.new(Vector3.new(x, y, z)))
+end
+
+UIS.InputBegan:Connect(function(input)
+if input.KeyCode == Enum.KeyCode.BackSlash then
+local pos = mouse.hit.p
+trap(pos.X, pos.Y, pos.Z)
+end end)
+elseif game.PlaceId == 636649648 then
+local plr = game.Players.LocalPlayer
+local mouse = plr:GetMouse()
+local UIS = game:GetService("UserInputService")
+function trap(x,y,z)
+game:GetService("ReplicatedStorage").TrapSystem.PlaceTrap:InvokeServer(CFrame.new(Vector3.new(x, y, z)))
+end
+
+UIS.InputBegan:Connect(function(input)
+if input.KeyCode == Enum.KeyCode.BackSlash then
+local pos = mouse.hit.p
+trap(pos.X, pos.Y, pos.Z)
+end end) end
